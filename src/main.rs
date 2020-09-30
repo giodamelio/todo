31ff55845f7e5fa7cd917e9962fd43ff8725dcad
Gio d'Amelio <giodamelio@gmail.com>
2020-09-30 21:28:09 +0000

    Print errors nicely when the app exits

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,27 @@ mod schema;
 
 use std::env;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::crate_version;
-use log::debug;
+use log::{debug, error, info};
 
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 
 fn database_connect() -> Result<SqliteConnection> {
-    let database_url = env::var("DATABASE_URL")?;
-    Ok(SqliteConnection::establish(&database_url).expect("Error connecting"))
+    let database_url =
+        env::var("DATABASE_URL").context("DATABASE_URL environment variable not set")?;
+    SqliteConnection::establish(&database_url).context("Could not connect to database")
 }
 
-fn main() -> Result<()> {
+fn go() -> Result<()> {
     let args = args::parse();
     pretty_env_logger::formatted_builder()
         .filter_level(args.log_level)
         .init();
 
     debug!("TODO version {}", crate_version!());
-    println!("Args: {:?}", args);
+    info!("Args: {:?}", args);
 
     // Connect to the database
     let connection = database_connect()?;
@@ -40,4 +41,16 @@ fn main() -> Result<()> {
         println!("{:?}", todo_item);
     }
     Ok(())
+}
+
+fn main() {
+    match go() {
+        Err(error) => {
+            error!("Error: {}", error);
+            for err in error.chain().skip(1) {
+                error!("    {}", err)
+            }
+        }
+        Ok(()) => {}
+    }
 }
