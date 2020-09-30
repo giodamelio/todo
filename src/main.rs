17ec9d13aa79d1b5963f2fb5df7a872ce0c58358
Gio d'Amelio <giodamelio@gmail.com>
2020-09-30 05:24:35 +0000

    Add basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod args;
 
+use clap::crate_version;
+use log::{debug, error, info, trace, warn};
+
 fn main() {
+    pretty_env_logger::init();
+
+    debug!("TODO version {}", crate_version!());
+
     let args = args::parse();
 
     println!("Args: {:?}", args);
